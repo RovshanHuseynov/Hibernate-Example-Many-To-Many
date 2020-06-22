2352f6d15d3572b01043caeff5edc6f70a69651d
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-06-22 04:10:34 +0400

    -- Not working

--- src/main/java/com/rh/spring/hibernateexamplemanytomany/entity/Laptop.java
+++ src/main/java/com/rh/spring/hibernateexamplemanytomany/entity/Laptop.java
@@ -1,6 +1,10 @@
 package com.rh.spring.hibernateexamplemanytomany.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "laptop")
@@ -12,4 +16,51 @@ public class Laptop {
 
     @Column(name = "name")
     private String name;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "laptop_student", joinColumns = @JoinColumn(name = "laptop_id")
+            , inverseJoinColumns = @JoinColumn(name = "student_id"))
+    @JsonIgnore
+    private List<Student> students = new ArrayList<>();
+
+    public Laptop() {
+    }
+
+    public Laptop(String name, List<Student> students) {
+        this.name = name;
+        this.students = students;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", students=" + students +
+                '}';
+    }
 }
--- src/main/java/com/rh/spring/hibernateexamplemanytomany/entity/Student.java
+++ src/main/java/com/rh/spring/hibernateexamplemanytomany/entity/Student.java
@@ -1,6 +1,10 @@
 package com.rh.spring.hibernateexamplemanytomany.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "student")
@@ -12,4 +16,49 @@ public class Student {
 
     @Column(name = "name")
     private String name;
+
+    @ManyToMany(mappedBy = "students", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Laptop> laptops = new ArrayList<>();
+
+    public Student() {
+    }
+
+    public Student(String name, List<Laptop> laptops) {
+        this.name = name;
+        this.laptops = laptops;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Laptop> getLaptops() {
+        return laptops;
+    }
+
+    public void setLaptops(List<Laptop> laptops) {
+        this.laptops = laptops;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", laptops=" + laptops +
+                '}';
+    }
 }
